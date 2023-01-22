3db3f26c454a2088d6ca9218aee6e77b024a6592
SatovaA <2919bpqpaola@gmail.com>
2023-01-22 11:21:20 -0500

    Se crea modelo de rutas y clientes

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -5,18 +5,18 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Route extends Model
+class Client extends Model
 {
     use HasFactory;
 
-    protected $table = 'routes';
+    protected $table = 'clients';
 
-    protected $primaryKey='idRoute';
+    protected $primaryKey='idClient';
 
     protected $fillable = [
-        'idCity',
-        'latitude',
-        'length',
-        'status',
+        'name',
+        'surname',
+        'phone',
+        'idRoute',
     ];
 }
