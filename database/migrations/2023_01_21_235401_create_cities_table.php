1ac7f22a5190c6e498441e5794e84d86f445f46a
SatovaA <2919bpqpaola@gmail.com>
2023-01-21 19:08:19 -0500

    Se crea migracion de ciudad, pais y rutas

---
+++ database/migrations/2023_01_21_235401_create_cities_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cities', function (Blueprint $table) {
+            $table->increments('idCity');
+            $table->string('name', 25);
+            $table->integer('idCountry')->unsigned();
+            $table->foreign('idCity')->references('idCountry')->on('countries');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cities');
+    }
+}
---
+++ database/migrations/2023_01_21_235842_create_routes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRoutesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('routes', function (Blueprint $table) {
+            $table->increments('idRoute');
+            $table->integer('idCity')->unsigned();
+            $table->foreign('idCity')->references('idCity')->on('cities');
+            $table->double('latitude');
+            $table->double('length');
+            $table->boolean('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('routes');
+    }
+}
