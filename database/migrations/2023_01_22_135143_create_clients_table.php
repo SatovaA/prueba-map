2e7b9bbd8482be8009322ceaedc943a34f557843
SatovaA <2919bpqpaola@gmail.com>
2023-01-22 11:20:25 -0500

    se crea migracion clientes

---
+++ database/migrations/2023_01_22_135143_create_clients_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->increments('idClient');
+            $table->string('name',25);
+            $table->string('surname',25);
+            $table->string('phone',10);
+            $table->integer('idRoute')->unsigned();
+            $table->foreign('idRoute')->references('idRoute')->on('routes');
+            $table->boolean('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+}
